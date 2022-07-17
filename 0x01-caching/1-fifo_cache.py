e46ebb25561f7fdc863904bdf66e15caaef935e4
Pericles001 <periclesadjovi@gmail.com>
2022-07-17 16:42:22 +0100

    Edit file with fifo cache algorithm

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+FIFO Caching
+"""
+
+
+from collections import deque
+
+
+BasicCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BasicCaching):
+    """
+    a class FIFOCache that inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """
+        Init method
+        """
+        super().__init__()
+        self.key_indexes = []
+
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+
+            if len(self.cache_data) == self.capacity:
+                self.cache_data.popitem(last=False)
+                print("DISCARD:", self.key_indexes.pop(0))
+
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key.
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None

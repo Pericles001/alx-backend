93453dd2d9e09cc161666040528e079c95f25ddf
Pericles001 <periclesadjovi@gmail.com>
2022-07-17 13:56:49 +0100

    .\0-basic_cache.py Update file

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Basic ditionary
+"""
+
+
+BasicCaching = __import__('base_caching').BasicCache
+
+
+class BasicCache(BasicCaching):
+    """
+    Basic Cache class that inherits from BasicCaching
+    """
+
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+
+    def get(self, key):
+        """
+        Must return the value in self.cache_data linked to key.
+        """
+        return self.cache_data.get(key, None)

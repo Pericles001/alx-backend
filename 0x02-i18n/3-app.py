f24a2fa59e73fa3c501e255862d07ee68457d507
Pericles001 <periclesadjovi@gmail.com>
2022-07-21 06:45:42 +0100

    Edit file

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
-"""
-Parametrize templates
+""" 3. Basic Flask app
 """
 
-import babel
 from flask import Flask, render_template, request
 from flask_babel import Babel
-
+s
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config:
+    """ Config class.
     """
-    Config class
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
@@ -25,19 +22,17 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """
-     determine the best match with our supported languages.
+    """ get_locale.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """
-    hello world
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello():
+    """ hello.
     """
     return render_template('3-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")

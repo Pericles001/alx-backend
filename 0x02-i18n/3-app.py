327ab470c546ce76d93821ff4d7bcd93c05c906e
Pericles001 <periclesadjovi@gmail.com>
2022-07-21 06:47:20 +0100

    Edit file

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-""" 3. Basic Flask app
+"""
+Parametrize templates
 """
 
+import babel
 from flask import Flask, render_template, request
 from flask_babel import Babel
-s
+
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config:
-    """ Config class.
     """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
@@ -22,17 +25,19 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """ get_locale.
+    """
+     determine the best match with our supported languages.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=["GET"], strict_slashes=False)
-def hello():
-    """ hello.
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
     """
     return render_template('3-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(debug=True)
